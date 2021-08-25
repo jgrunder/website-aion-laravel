a36377fb9515b70c0a2a789c485a7053387da541
Jonathan57500 <jonathan.grunder@gmail.com>
2021-08-25 18:18:20 +0200

    Add custom databases connexions

--- config/database.php
+++ config/database.php
@@ -35,61 +35,41 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        'gameserver' => [
+            'driver'        => 'mysql',
+            'host'          => env('DB_HOST_GS', '127.0.0.1'),
+            'database'      => env('DB_DATABASE_GS', 'forge'),
+            'username'      => env('DB_USERNAME_GS', 'forge'),
+            'password'      => env('DB_PASSWORD_GS', ''),
+            'charset'       => 'utf8',
+            'collation'     => 'utf8_unicode_ci',
+            'prefix'        => '',
+            'strict'        => false
         ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+        
+        'loginserver' => [
+            'driver'        => 'mysql',
+            'host'          => env('DB_HOST_LS', '127.0.0.1'),
+            'database'      => env('DB_DATABASE_LS', 'forge'),
+            'username'      => env('DB_USERNAME_LS', 'forge'),
+            'password'      => env('DB_PASSWORD_LS', ''),
+            'charset'       => 'utf8',
+            'collation'     => 'utf8_unicode_ci',
+            'prefix'        => '',
+            'strict'        => false
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
+        
+        'webserver' => [
+            'driver'        => 'mysql',
+            'host'          => env('DB_HOST_WS', '127.0.0.1'),
+            'database'      => env('DB_DATABASE_WS', 'forge'),
+            'username'      => env('DB_USERNAME_WS', 'forge'),
+            'password'      => env('DB_PASSWORD_WS', ''),
+            'charset'       => 'utf8',
+            'collation'     => 'utf8_unicode_ci',
+            'prefix'        => '',
+            'strict'        => false
+        ]
 
     ],
 
