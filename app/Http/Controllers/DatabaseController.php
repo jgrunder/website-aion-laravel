cb16e70883aaa7617ff60395dd9772c97a658487
Jonathan57500 <jonathan.grunder@gmail.com>
2021-09-21 22:23:17 +0200

    Fix issue on retrieve item from database

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -19,20 +19,24 @@ class DatabaseController extends Controller {
         if(Cache::has($key)){
             echo Cache::get($key);
         }
-        elseif(config('aion.aion_version') == '2.7') {
+        else {
             
-            $value = file_get_contents('https://aiondatabase.info/'.Cookie::get('language').'/tip/'.$id);
-            $value = str_replace('src="/', 'src="https://aiondatabase.info/', $value);
+            if(config('aion.aion_version') == '2.7') {
+                
+                $value = file_get_contents('https://aiondatabase.info/'.Cookie::get('language').'/tip/'.$id);
+                $value = str_replace('src="/', 'src="https://aiondatabase.info/', $value);
+                
+            } else {
+                
+                $value = file_get_contents('http://aiondatabase.net/tip.php?id=item--'.$id.'&l='.Cookie::get('language').'&nf=on');
+                $value = str_replace('src="/', 'src="http://aiondatabase.net/', $value);
+                
+            }
             
-        } else {
-
-            $value = file_get_contents('http://aiondatabase.net/tip.php?id=item--'.$id.'&l='.Cookie::get('language').'&nf=on');
-            $value = str_replace('src="/', 'src="http://aiondatabase.net/', $value);
-
             Cache::forever($key, $value);
-
             echo $value;
         }
+            
     }
 
 }
