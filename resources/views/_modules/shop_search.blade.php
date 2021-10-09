2705f9014bbab7b04db41f399908c06d47bc45dd
Jonathan57500 <jonathan.grunder@gmail.com>
2021-10-09 18:00:56 +0200

    Fix search in shop

--- resources/views/_modules/shop_search.blade.php
+++ resources/views/_modules/shop_search.blade.php
@@ -3,11 +3,17 @@
 					<h2>{!! Lang::get('all.shop.search_title') !!}</h2>
 				</div>
 				<div class="bloc_body center container_shop_categories">
-					{!! Form::open(['class' => 'navbar-form navbar-right', 'url' => Route('shop.search'), 'method' => 'get']) !!}
-					<div class="form-group">
-						{!! Form::text('search_value', null, ['placeholder' => "Item", 'class' => 'input']) !!}
-						<p><input type="submit" class="btn" value="{!! Lang::get('all.shop.search_btn') !!}" /></p>
-					</div>
-					{!! Form::close() !!}
+                    <table>
+                        <tr><td>
+    					{!! Form::open(['class' => 'navbar-form navbar-right', 'url' => Route('shop.search'), 'method' => 'get']) !!}
+    					<div class="form-group">
+    						{!! Form::text('search_value', null, ['placeholder' => "Item", 'class' => 'input']) !!}
+                        </div>
+                        <div class="form-group" style="margin-top: 5px;">
+    						<p><input type="submit" class="btn" value="{!! Lang::get('all.shop.search_btn') !!}" /></p>
+    					</div>
+    					{!! Form::close() !!}
+                        </td></tr>
+                    </table>
 				</div>
             </div>
